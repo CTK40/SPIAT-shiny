db0c2c88523f7002d2901e0c3e560a820891491c
fuerzhou <yuzhou0610@gmail.com>
2023-05-30 13:21:03 +1000

    Up

--- Tabs/Tab3.R
+++ Tabs/Tab3.R
@@ -1,20 +1,10 @@
-library(shinyFiles)
 Tab3 <- tabPanel(
     "Plot marker intensities",
     fluidPage(
-        sidebarLayout(
-            sidebarPanel(
-                # load object
-                # fileInput("spe_object", "Choose spe object file",
-                #           accept = c(".Rda", ".rda", ".RData", ".RDS"))
-
-            ),
-            mainPanel(
-                # Show part of the table
-                # Select columns for genes/markers    
-                uiOutput('marker_level'),
-                plotOutput("plot_marker", width = "600px", height = "600px")
-            )
+        mainPanel(
+            # Select columns for genes/markers    
+            uiOutput('marker_level'),
+            plotOutput("plot_marker", width = "600px", height = "600px")
         )
     )
 )
--- Tabs/load_object.R
+++ Tabs/load_object.R
@@ -2,10 +2,7 @@ library(shinyFiles)
 load_spe_object <- tabPanel(
     "Load object",
     fluidPage(
-        # fileInput("spe_object", "Choose spe object file",
-        #           accept = c(".Rda", ".rda", ".RData", ".RDS"))
         shinyFilesButton("Btn_GetFile", "Choose a file" ,
-                         title = "Please select a file:", multiple = FALSE,
-                         buttonType = "default", class = NULL)
+                         title = "Please select a file:", multiple = FALSE)
     )
 )
